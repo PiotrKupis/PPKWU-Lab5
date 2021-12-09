70ac912e46d593729c77eea09c6c823e5f16d8d1
Piotr Kupis <piotr.kupis42@gmail.com>
2021-12-09 12:51:51 +0100

    add getting data from https://panoramafirm.pl/

--- src/main/java/com/example/vcard/VCardController.java
+++ src/main/java/com/example/vcard/VCardController.java
@@ -1,5 +1,12 @@
 package com.example.vcard;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,11 +15,33 @@ import org.springframework.web.bind.annotation.RestController;
 public class VCardController {
 
     @GetMapping("/vCard/{profession}")
-    public String getData(@PathVariable("profession") String profession){
+    public String getData(@PathVariable("profession") String profession) throws IOException {
+
+        String url = String.format("https://panoramafirm.pl/szukaj=%s", profession);
+        System.out.println(url);
+        Document document = Jsoup.connect(url).get();
+
+        List<Business> businesses = new ArrayList<>();
+        Elements elements = document.select("script");
+        for (Element element : elements) {
+            if (element.attr("type").equals("application/ld+json")) {
+                if (element.data().contains("LocalBusiness")) {
+                    Business business = new Business();
+                    business.setName(getField(element.data(), "name"));
+                    business.setTelephone(getField(element.data(), "telephone"));
+                    business.setEmail(getField(element.data(), "email"));
+                    business.setPostalCode(getField(element.data(), "postalCode"));
+                    business.setStreetAddress(getField(element.data(), "streetAddress"));
+                    business.setCity(getField(element.data(), "addressLocality"));
+                    businesses.add(business);
+                }
+            }
+        }
+
+        businesses.forEach(System.out::println);
 
         StringBuilder page = new StringBuilder();
         page.append("<html><header><title>VCard</title></header><body>");
-        page.append("test");
 
         page.append("<table><tr>");
         page.append("<th>Nazwa</th>");
@@ -22,7 +51,12 @@ public class VCardController {
 
         page.append("</body></html>");
 
-
         return page.toString();
+    }
+
+    private String getField(String json, String field) {
+        int indexOf = json.indexOf(field);
+        return json.substring(indexOf + field.length() + 3,
+            json.indexOf("\"", indexOf + field.length() + 3));
     }
 }
